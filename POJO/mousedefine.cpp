407bcaf7b883042ff8a495360e7594845747dd1a
fuyuning-233 <fuyuning1212@stu.scu.edu.cn>
2024-07-01 14:17:58 +0800

    key/mouse补全

--- POJO/mousedefine.cpp
+++ POJO/mousedefine.cpp
@@ -1,6 +1,34 @@
-//#include "mousedefine.h"
+#include "mousedefine.h"
 
-//MouseDefine::MouseDefine()
-//{
+MouseDefine::MouseDefine(){}
+MouseDefine::MouseDefine(int keyType,int time){
+    this->keyType=keyType;
+    this->time=time;
+}
+int MouseDefine::getKeyType(){
+    return keyType;
+}
 
-//}
+int MouseDefine::gettime(){
+    return time;
+}
+
+string MouseDefine::getName(){
+    return name;
+}
+
+void MouseDefine::setName(string name){
+    this->name=name;
+}
+
+void MouseDefine::setKeyType(int type){
+    keyType=type;
+}
+
+void MouseDefine::setTime(int time){
+    this->time=time;
+}
+
+void MouseDefine:: save(){
+
+}
