a03a94c9618d46a4030585526b1889a07e65b3fb
hpyyyyyy <XY0017124@outlook.com>
2024-07-15 18:20:13 +0800

    删除了调试内容，需要在temp2下才可正常运行

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,27 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    WinKeyHook* start=new WinKeyHook;
+    start->SetKeyPressCallBack(std::bind(&MainWindow::keyPressEvent, this, std::placeholders::_1));
+
+    //界面切换
+    triger_widget *page1;
+    add_widget *page2;
+    set_widget *page3;
+
+    page1=new triger_widget(this);
+    page2=new add_widget(this);
+    page3=new set_widget(this);
+
+    ui->stackedWidget->insertWidget(0,page1);
+    ui->stackedWidget->insertWidget(1,page2);
+    ui->stackedWidget->insertWidget(2,page3);
+
+    ui->stackedWidget->setCurrentIndex(0);
+
+
 }
-KeyDefine key;
+
 MainWindow::~MainWindow()
 {
     delete ui;
@@ -16,25 +35,29 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_triggerBtn_clicked()
 {
-<<<<<<< Updated upstream
-=======
-    //ui->stackedWidget->setCurrentIndex(0);
-    key.regord();
->>>>>>> Stashed changes
-
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 void MainWindow::on_addBtn_clicked()
 {
-<<<<<<< Updated upstream
-=======
-    //ui->stackedWidget->setCurrentIndex(1);
-    key.end();
->>>>>>> Stashed changes
-
+     ui->stackedWidget->setCurrentIndex(1);
 }
 
 void MainWindow::on_setBtn_clicked()
 {
-
+     ui->stackedWidget->setCurrentIndex(2);
 }
+
+void MainWindow::keyPressEvent(int keyCode)
+{
+    if(keyCode==Applycation::getStartKey())
+    {
+        DefineTrigerThread::setFlag(true);
+        qDebug()<<"start shortcut pressed";
+    }else if(keyCode==Applycation::getEndKey()){
+        DefineTrigerThread::setFlag(false);
+        qDebug()<<"end shortcut pressed";
+    }
+}
+
+
