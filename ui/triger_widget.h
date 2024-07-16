214095fa3d963f6f95c7535ee08cba229a0ef448
yuanhao10288 <2936924721@qq.com>
2024-07-16 09:26:19 +0800

    触发更改

--- ui/triger_widget.h
+++ ui/triger_widget.h
@@ -10,11 +10,11 @@
 #include<QApplication>
 #include<QButtonGroup>
 #include<QDebug>
+#include<QPushButton>
 #include"applycation.h"
 #include"../POJO/mydefine.h"
-//#include"../myThread/definetrigerthread.h"
 #include"definetrigerthread.h"
-
+//#include"mainwindow.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class triger_widget; }
 QT_END_NAMESPACE
@@ -27,6 +27,10 @@ public:
     explicit triger_widget(QWidget *parent = nullptr);
     ~triger_widget();
     int choice=0;          //选择触发哪一个宏
+    vector<myDefine*>* list=Applycation::getDefineList();
+
+private slots:
+    void on_deleteBtn_clicked();
 
 private:
     Ui::triger_widget *ui;
